4b2463a351d6576876f36bdb20a9679614bd2d39
medi772425 <75742857+medi772425@users.noreply.github.com>
2021-08-27 05:32:59 +0900

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
-import React from "react";
+// esliintの機能をページ単位？でオフにできる。
+/* eslint react-hooks/exhaustive-deps:off */
+
+import React, { useEffect, useState } from "react";
 import Colorfulmessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => {
-    alert("a");
+  console.log("App");
+  const [num, setNum] = useState(0);
+  const [showFlag, setShowFlag] = useState(false);
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const onClickSwitchShow = () => {
+    // 今のshowFlagの逆をセットする
+    setShowFlag(!showFlag);
+  };
+
+  // 第2引数の配列に指定した値が変更された場合、第１引数の関数が実行される。
+  // 第2引数の配列に何も指定しなければ、最初にレンダリングされたときだけ実行される。
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        showFlag || setShowFlag(true);
+      } else {
+        showFlag && setShowFlag(false);
+      }
+    }
+    // 第2引数の配列に、条件となるshowFlagを書いていないと、eslintからメッセージがでる。
+    // ただし、今回の場合はあえてshowflagを見ていないので、エラーは無視する。設定でoffにもできる。
+  }, [num]);
 
   return (
     <>
       <h1 style={{ color: "red" }}>こんにちは</h1>
       <Colorfulmessage color="orange">お元気ですか！</Colorfulmessage>
-      <Colorfulmessage color="pink">お元気ですか2！</Colorfulmessage>
-      <button onClick={onClickButton}>ボタン</button>
+      {/* <Colorfulmessage color="pink">お元気ですか2！</Colorfulmessage> */}
+      <button onClick={onClickCountUp}>ボタン</button>
+      <br />
+      <button onClick={onClickSwitchShow}>on/off</button>
+      <p>{num}</p>
+      {/* 左辺がtrueの場合に、右を返す */}
+      {showFlag && <span>aaaaaaaa</span>}
     </>
   );
 };
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 const ColorfulMessage = (props) => {
+  console.log("カラフル");
+
   // タグ内の文字列はchildrenプロパティに格納される
   const { color, children } = props;
 
